57ba0fbcd928c4f0af8a630b088a6fb821745b57
AstridYovera <ayoveratinoco@gmail.com>
2023-09-21 17:37:49 -0500

    Registro Tesis - finalizado

--- src/main/java/com/centroinformacion/controller/TesisRegistroController.java
+++ src/main/java/com/centroinformacion/controller/TesisRegistroController.java
@@ -5,10 +5,13 @@ package com.centroinformacion.controller;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -43,4 +46,15 @@ public class TesisRegistroController {
 		return map;
 	}
 	
+	
+	@GetMapping("/buscaPorTituloOrTemaTesis")
+	@ResponseBody
+	public String validaTituloOrTema(String titulo, String tema) {
+		List<Tesis> lstTesis = tesisService.listaPorTituloOrTema(titulo, tema);
+		if (CollectionUtils.isEmpty(lstTesis)) {
+			return "{\"valid\" : true }";
+		} else {
+			return "{\"valid\" : false }";
+		}
+	}
 }
--- src/main/java/com/centroinformacion/repository/TesisRepository.java
+++ src/main/java/com/centroinformacion/repository/TesisRepository.java
@@ -1,9 +1,11 @@
 package com.centroinformacion.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.centroinformacion.entity.Tesis;
 
 public interface TesisRepository extends JpaRepository<Tesis, Integer> {
-
-}
+	public List<Tesis> findByTituloOrTemaIgnoreCase(String titulo, String tema);
+	}
